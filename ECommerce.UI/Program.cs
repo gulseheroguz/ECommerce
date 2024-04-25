d23780c0b8294bef00ae5be6a3504c0f3bfcce8a
305-öğrenci <305-renci@DESKTOP-PO3KMEU>
2024-04-25 13:26:25 +0300

    Add project files.

---
+++ ECommerce.UI/Program.cs
@@ -0,0 +1,67 @@
+using ECommerce.Data.Context;
+using ECommerce.Data.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Dependency Injection
+builder.Services.AddDbContext<ECommerceContext>();
+
+//Identity
+builder.Services.AddIdentity<AppUser, AppRole>(options =>
+{
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 5;
+}).AddEntityFrameworkStores<ECommerceContext>()
+.AddDefaultTokenProviders();
+
+
+builder.Services.ConfigureApplicationCookie(option =>
+{
+    option.LoginPath = "/Acoount/Login";
+    option.AccessDeniedPath = "/";
+    option.Cookie = new CookieBuilder
+    {
+        Name = "AspNetCoreIdentityExampleCookie",
+        HttpOnly = false,
+        SameSite = SameSiteMode.Lax,
+        SecurePolicy = CookieSecurePolicy.None
+    };
+});
+
+
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization(); 
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
